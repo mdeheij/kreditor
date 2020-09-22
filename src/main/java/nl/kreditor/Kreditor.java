e18b151b88db10f3b5e9ce32825052b512875cf2
Mike de Heij <mdeheij@hostnet.nl>
2020-09-22 15:56:48 +0200

    Add initial SpringBootApplication class

---
+++ src/main/java/nl/kreditor/Kreditor.java
@@ -0,0 +1,11 @@
+package nl.kreditor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kreditor {
+    public static void main(String[] args) {
+        SpringApplication.run(Kreditor.class, args);
+    }
+}
