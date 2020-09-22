a329a652d6d7ee4634c4b916cf62a53c638b16ee
Mike de Heij <mdeheij@hostnet.nl>
2020-09-22 15:56:48 +0200

    Add EntityScan and User model

--- src/main/java/nl/kreditor/Kreditor.java
+++ src/main/java/nl/kreditor/Kreditor.java
@@ -2,8 +2,10 @@ package nl.kreditor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = {"nl.kreditor.model"})
 public class Kreditor {
     public static void main(String[] args) {
         SpringApplication.run(Kreditor.class, args);
---
+++ src/main/java/nl/kreditor/model/User.java
@@ -0,0 +1,44 @@
+package nl.kreditor.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String username, String password) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Column
+    private LocalDateTime disabledAt;
+
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
